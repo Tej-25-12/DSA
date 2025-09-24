d06de091ec6b2fd4cfda93bde9b36544574f04f3
Tejas <tejasjadhav25122005@gmail.com>
2025-09-24 19:21:38 +0530

    Time: 1 ms (84.55%) | Memory: 41.3 MB (20.85%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = input.nextInt();
+        Solution solution = new Solution();
+        int output = solution.mySqrt(x);
+        System.out.println("The square root of "+ x + " is "+ output);
+    }
+    public int mySqrt(int x) {
+        if(x < 2) {
+            return x;
+        }
+        int left =1;
+        int right =x;
+        int ans =0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long midSquared = (long) mid * mid;
+            if (midSquared == x) {
+                return mid;
+            }else if (midSquared < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1; // Too big, try lower
+            }
+        }
+
+        return ans; // Floor of sqrt(x)
+    }
+}
